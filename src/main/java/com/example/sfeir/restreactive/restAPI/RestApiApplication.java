8772d8923b9d514c79ce3e99c8b7131339de92a0
COSSA Hugo <hugo@mbp-de-cossa.home>
2022-08-09 15:19:53 +0200

    Modify main class such that we exclude the datasource autoconfiguration

--- src/main/java/com/example/sfeir/restreactive/restAPI/RestApiApplication.java
+++ src/main/java/com/example/sfeir/restreactive/restAPI/RestApiApplication.java
@@ -2,10 +2,10 @@ package com.example.sfeir.restreactive.restAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class RestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
